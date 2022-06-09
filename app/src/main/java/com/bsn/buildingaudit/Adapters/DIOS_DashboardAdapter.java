bf05a9b1d4cc2aa31b3b012d2a5904d72eebb46c
BSNShivam <shivamsavita1007@gmail.com>
2022-06-09 18:59:05 +0530

    dios dashboard ready

--- app/src/main/java/com/bsn/buildingaudit/Adapters/DIOS_DashboardAdapter.java
+++ app/src/main/java/com/bsn/buildingaudit/Adapters/DIOS_DashboardAdapter.java
@@ -1,14 +1,18 @@
 package com.bsn.buildingaudit.Adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bsn.buildingaudit.DIOS.Diios_Panel_Under_School_dashboard;
 import com.bsn.buildingaudit.R;
 
 import java.util.ArrayList;
@@ -25,14 +29,33 @@ public class DIOS_DashboardAdapter extends RecyclerView.Adapter<DIOS_DashboardAd
     @NonNull
     @Override
     public dashboardRecViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.dashboard_review_itemcard,parent,false);
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.dashboard_dios_itemcard,parent,false);
         return new dashboardRecViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull dashboardRecViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull dashboardRecViewHolder holder, @SuppressLint("RecyclerView") int position) {
         holder.roomTypetxt.setText(arrayListSchool.get(position));
         holder.updateOntxt.setText("12-15-2022 02:30:28 PM");
+        if (position%2==0){
+
+            holder.penddingAndSDoneBtn.setImageDrawable(context.getDrawable(R.drawable.ic_right_icon));
+        }
+        else{
+            holder.penddingAndSDoneBtn.setImageDrawable(context.getDrawable(R.drawable.ic_wron_icon));
+
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (position%2==1){
+
+                  context.startActivity(new Intent(context, Diios_Panel_Under_School_dashboard.class));
+                }
+            }
+        });
+
     }
 
     @Override
@@ -61,11 +84,13 @@ public class DIOS_DashboardAdapter extends RecyclerView.Adapter<DIOS_DashboardAd
 
     public class dashboardRecViewHolder extends RecyclerView.ViewHolder {
         TextView updateOntxt,roomTypetxt;
+        ImageView penddingAndSDoneBtn;
 
         public dashboardRecViewHolder(@NonNull View itemView) {
             super(itemView);
             updateOntxt=itemView.findViewById(R.id.updateOntxt);
             roomTypetxt=itemView.findViewById(R.id.roomTypetxt);
+            penddingAndSDoneBtn=itemView.findViewById(R.id.penddingAndSDoneBtn);
         }
     }
 }
---
+++ app/src/main/java/com/bsn/buildingaudit/DIOS/Diios_Panel_Under_School_dashboard.java
@@ -0,0 +1,16 @@
+package com.bsn.buildingaudit.DIOS;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bsn.buildingaudit.R;
+
+public class Diios_Panel_Under_School_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diios_panel_under_school_dashboard);
+    }
+}
